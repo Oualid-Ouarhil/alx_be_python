a552b16257a013dc94df945ddb148b69c7c53f70
Oualid-Ouarhil <ouarhiloualid@gmail.com>
2024-06-30 21:47:48 +0100

    Initial change

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,17 @@
+size = int(input('ENTER THE SIZE OF THE PATTERN:'))
+
+count = 0
+
+while count < size:
+
+    for i in range(size):
+        print('*', end='')
+    print()
+    count += 1
+
+
+
+    for i in range(size):
+        print('*', end='')
+    print() 
+    count += 1
