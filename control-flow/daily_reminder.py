df9b5b7c6febf0e3733371e4ea4b69f8848fa135
Oualid-Ouarhil <ouarhiloualid@gmail.com>
2024-06-30 22:40:35 +0100

    inital change

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,27 +1,22 @@
-task = input("Enter your task:")
-priority = input("Priority (high/medium/low):")
-time_intens = input("Is it time-bound? (yes/no):")
-error_mess = "something went wrong!"
-first_part = f"{task} is a {priority} priority task"
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
+reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
+low_reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
 
 match priority:
-    case "high":
-        reminder = "that requires immediate attention today!"
-    case "medium":
-       reminder = "schedule it for later"
-    case "low":
-        reminder = "Consider completing it when you have free time."
-    case _ :
-        message = error_mess
-
-if time_intens == "yes":
-    reminder = "that requires immediate attention today!"
-elif time_intens == "no":
-    reminder = "Consider completing it when you have free time."
-else:
-    message = error_mess
-
-message = (f"Reminder: '{task}' is a {priority} priority task. that requires immediate attention today!" 
-           if time_intens == "yes" else "Consider completing it when you have free time.")
-print("Reminder: ", reminder)
-print(message)
+    case "high" : 
+        if time_bound == "yes":
+            print("Reminder:",reminder)
+        else:
+            print("Note:",low_reminder)
+    case "medium": 
+        if time_bound == "yes":
+            print("Reminder:",reminder)
+        else:
+            print("Note:",low_reminder)
+    case "low": 
+        if time_bound == "yes":
+            print("Reminder:",reminder)
+        else:
+            print("Note:",low_reminder)
