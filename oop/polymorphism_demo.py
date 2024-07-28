995aace948cf6d76daab0dcbe6920954e1abaced
Oualid-Ouarhil <ouarhiloualid@gmail.com>
2024-07-28 17:21:41 +0100

    initial change

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,20 @@
+class shape:
+    def area(self):
+        raise NotImplementedError
+
+class rectangle(shape):
+    def __init__(self, length, width)
+        self.length = length
+        self.width = width
+        
+    def area(self):
+        return self.length * self.width
+
+class circle(shape):
+    def __init__(self, radius)
+        self.radius = radius
+        
+    def area(self):
+        import math
+        return math.pi * self.radius **2 
+
